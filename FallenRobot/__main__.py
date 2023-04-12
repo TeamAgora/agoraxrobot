9d3b9b4b06f76b714b51052984cd0aa6dc8c2ad5
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-12 18:31:13 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -205,7 +205,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAx0CYRUl4wAC_9lkNqrOYG4n2oeu1A91ELTdokVvXwACyQoAAj2ysVW3JYSUZlyEpy8E"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
