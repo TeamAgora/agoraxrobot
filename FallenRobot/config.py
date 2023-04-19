72ae103771077dea1def920ba6655e8988db65d0
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-04-19 16:19:20 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,7 +14,7 @@ class Config(object):
     MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
-    START_IMG = "https://telegra.ph/file/8c78951f44aa036252af9.jpg"
+    START_IMG = "https://graph.org/file/6ed3863209d0be9c968de.jpg"
 
     SUPPORT_CHAT = "KIMJIKOINSUPPORT"  # Your Telegram support group chat username where your users will go and bother you
 
